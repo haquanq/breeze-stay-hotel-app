f54c6950dca606e05b60768e89fafa9720806b21
Ha Quanq <quangha0120010@gmail.com>
2025-11-08 02:21:22 +0700

    Add Spring Boot Application to workspace

---
+++ apps/backend/src/main/java/haquanq/com/backend/HotelBackendApplication.java
@@ -0,0 +1,12 @@
+package haquanq.com.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelBackendApplication.class, args);
+	}
+}
